83a71078377b1854ad6aeeef4dc702c210a92e0d
anthonyrom <14960401+anthonyrom@users.noreply.github.com>
2020-09-23 03:13:27 -0400

    successful basic intermodulation
    two versions of AM present

--- testbed.m
+++ testbed.m
@@ -1,19 +1,71 @@
 % Read in audio file:
+filename = 'strummed_chords';
+ext = '.flac'; % audio file extension
+
 %   y : samples, double
 %   Fs: sample rate, double
-[y, Fs] = audioread('audio_samples/strummed_chords.flac');
+[y, Fs] = audioread(strcat('audio_samples/', filename, ext));
 
-% Playback audio file
-sound(y, Fs)
+% Create time index (useful later)
+% time: time of audio sample in seconds
+tot_samples = size(y, 1); % total num. samples
+seconds = tot_samples/Fs; % total length, seconds
+time = linspace(0, tot_samples/Fs, tot_samples); 
 
-% Time domain plot of signal
-timePlot(y)
+% Playback original audio file
+% sound(y, Fs)
+
+% Time domain plot of original file
+% timePlot(time, y)
+
+% modulation: two approaches
+freq = 8; % Hz
+
+% manual approach
+% output signal is z
+carrier = zeros(size(y(:,1)));
+z = zeros(size(y));
+for i = 1:size(carrier) 
+    % abs. value used so output sign is unchanged,
+    % and the modulation affects only magnitude
+    carrier(i) = sin(freq*2*pi*time(i));
+    z(i, :) = carrier(i)*y(i, :);
+end
+
+% built-in way, overwrites above
+% the two methods sound different (but similar)
+w = zeros(size(y));
+w = ammod(y, freq, Fs);
+
+% z is created manually, w by ammod()
+% this code shows they are not identical
+identicalAM = isequal(z, w);
+disp(identicalAM)
+
+% ammod does suppressed carrier AM
+% my manual method did not
+% so the difference is having the carrier freq. in the output
+
+% plot both vs original signal
+fullPlot(time, y, z, w)
+
+% play result
+sound(w, Fs)
 
 % Functions
 
-function timePlot(x)
+function fullPlot(time, x, y, z)
+    plot(time, x)
+    hold on
+    plot(time, y)
+    hold on
+    plot(time, z)
+    hold off
+end
+
+function timePlot(time, x)
 % Simple time domain plot of audio samples
     figure()
     clf
-    plot(x)
+    plot(time, x)
 end
