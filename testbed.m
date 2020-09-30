0897f83ee080a8117a0bc0b79133d019843e1fed
anthonyrom <14960401+anthonyrom@users.noreply.github.com>
2020-09-30 09:51:33 -0400

    More testing + added figures

--- testbed.m
+++ testbed.m
@@ -1,71 +1,78 @@
 % Read in audio file:
-filename = 'strummed_chords';
+filename = 'slide_bend_vibrato';
 ext = '.flac'; % audio file extension
 
 %   y : samples, double
-%   Fs: sample rate, double
+%   Fs: sampling frequency, double
 [y, Fs] = audioread(strcat('audio_samples/', filename, ext));
 
-% Create time index (useful later)
-% time: time of audio sample in seconds
-tot_samples = size(y, 1); % total num. samples
-seconds = tot_samples/Fs; % total length, seconds
-time = linspace(0, tot_samples/Fs, tot_samples); 
+% Useful stuff
+T = 1/Fs; % sampling period
+L = length(y); % number of samples
+time = (0:L-1)*T; % time values for each sample
 
-% Playback original audio file
+% Play back original audio file
 % sound(y, Fs)
 
 % Time domain plot of original file
 % timePlot(time, y)
 
-% modulation: two approaches
-freq = 8; % Hz
+% amplitude/intermodulation: two approaches
+freq = 300; % Hz
 
 % manual approach
 % output signal is z
-carrier = zeros(size(y(:,1)));
+carrier = zeros(size(y));
 z = zeros(size(y));
-for i = 1:size(carrier) 
+for i = 1:L
     % abs. value used so output sign is unchanged,
     % and the modulation affects only magnitude
     carrier(i) = sin(freq*2*pi*time(i));
     z(i, :) = carrier(i)*y(i, :);
 end
 
-% built-in way, overwrites above
+% built-in way
+% output signal is w
 % the two methods sound different (but similar)
-w = zeros(size(y));
 w = ammod(y, freq, Fs);
 
 % z is created manually, w by ammod()
 % this code shows they are not identical
 identicalAM = isequal(z, w);
-disp(identicalAM)
+% disp(identicalAM)
 
-% ammod does suppressed carrier AM
-% my manual method did not
+% ammod does suppressed carrier AM (DSB-SC)
+% my manual method did not (DSBAM)
 % so the difference is having the carrier freq. in the output
 
-% plot both vs original signal
-fullPlot(time, y, z, w)
+% compare a few versions of intermodulation
+test1 = ammod(y, 5, Fs);
+test2 = ammod(y, 200, Fs);
+test3 = ammod(y, 1000, Fs);
+
+% plot various IM versions
+fullPlot(time, test1(:,1), test2(:,1), test3(:,1))
 
 % play result
-sound(w, Fs)
+% sound(w, Fs)
 
 % Functions
 
 function fullPlot(time, x, y, z)
-    plot(time, x)
-    hold on
-    plot(time, y)
-    hold on
-    plot(time, z)
-    hold off
+    plot(time, x, time, y, time, z)
+    title('Intermodulated Signals with Various Carrier Frequencies')
+    xlabel('Time (s)')
+    ylabel('Normalized Audio Amplitude')
+    legend({'f = 5 Hz', 'f = 200 Hz', 'f = 1000 Hz'})
+    xlim([0.2 0.3])
 end
 
 function timePlot(time, x)
 % Simple time domain plot of audio samples
-    figure()
-    clf
     plot(time, x)
+    title('Original Audio Signal vs. Time')
+    xlabel('Time (s)')
+    ylabel('Normalized Audio Amplitude')
+    legend({'Audio Signal'})
+    xlim([0 0.5])
 end
